139300cef24d5bfb7690af47203164330f7f3fd6
somniumry <somniumry>
2021-01-24 18:41:58 +0900

    [Fix] 회원정보 gitbook 업데이트

--- server/controllers/users/update.js
+++ server/controllers/users/update.js
@@ -9,36 +9,68 @@ module.exports = async (req, res) => {
     const { userName, password } = req.body
 
     if (!req.headers.authorization) {
-        res.status(400).send({ message: 'invalid accessToken' })
+        res.status(400).send({ message: 'accessToken not found' })
     } else {
         const token = req.headers.authorization.split(" ")[2];
 
         try {
             const tokenVerify = jwt.verify(token, ACCESS_SECRET);
-
+            const date = new Date(parseInt(tokenVerify.exp) * 1000).toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })
             if (userName && password) {
                 const hash = await bcrypt.hash(req.body.password, saltRounds);
                 await users.update({ userName, password: hash }, { where: { email: tokenVerify.email } })
-                res.status(200).send({
-                    message: 'userName And Password update success'
-                })
+                const userInfo = await users.findOne({ where: { email: tokenVerify.email } })
+                res.status(200)
+                    .send({
+                        data: {
+                            id: userInfo.dataValues.id,
+                            userName: userInfo.dataValues.userName,
+                            email: userInfo.dataValues.email,
+                            auh: {
+                                token: token,
+                                expDate: date
+                            }
+                        },
+                        message: 'userName And Password update success'
+                    })
             } else if (userName) {
                 await users.update({ userName }, { where: { email: tokenVerify.email } })
-                res.status(200).send({
-                    message: 'userName update success'
-                })
+                const userInfo = await users.findOne({ where: { email: tokenVerify.email } })
+                res.status(200)
+                    .send({
+                        data: {
+                            id: userInfo.dataValues.id,
+                            userName: userInfo.dataValues.userName,
+                            email: userInfo.dataValues.email,
+                            auh: {
+                                token: token,
+                                expDate: date
+                            }
+                        },
+                        message: 'userName update success'
+                    })
             } else if (password) {
                 const hash = await bcrypt.hash(req.body.password, saltRounds);
                 await users.update({ password: hash }, { where: { email: tokenVerify.email } })
-                res.status(200).send({
-                    message: 'password update success'
-                })
+                const userInfo = await users.findOne({ where: { email: tokenVerify.email } })
+                res.status(200)
+                    .send({
+                        data: {
+                            id: userInfo.dataValues.id,
+                            userName: userInfo.dataValues.userName,
+                            email: userInfo.dataValues.email,
+                            auh: {
+                                token: token,
+                                expDate: date
+                            }
+                        },
+                        message: 'password update success'
+                    })
             } else {
                 res.status(400).send({ message: 'update failed' })
             }
         } catch (err) {
-            console.log(err)
-            res.status(400).send({ err: err, message: '유효하지 않는 accessToken' })
+            res.status(500).send({ message: 'server error' })
         }
     }
 }
