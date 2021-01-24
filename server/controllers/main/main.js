13fabb5d32b0ab489d1308fcab880e2a796bf6dc
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2021-01-24 20:13:11 +0900

    [feature]#7 /main 기능구현 완료

--- server/controllers/main/main.js
+++ server/controllers/main/main.js
@@ -1,3 +1,40 @@
+const { workouts, image, parts } = require('../../models'); 
+
 module.exports = async(req,res) =>{
+    try{
+        const workoutData = await workouts.findAll({
+            attributes : [
+                'id', 'title', 'instruction', 
+                'setCount', 'count', 'breakTime', 
+                'calrorie', 'category', 'tool'],
+        })
     
+        const _url = await image.findAll({
+            attributes : ['url','workoutId'], 
+        })
+    
+        const _parts = await workouts.findAll({
+            attributes : ['id'],
+            include : {model : parts}
+        })
+        
+         const result = workoutData.map((data,index)=>{
+            data.dataValues.image = new Array;
+            data.dataValues.parts = new Array;
+            _url.forEach(el=>{
+                if(el.workoutId === index+1){
+                    data.dataValues.image.push(el.url);
+                }
+            })
+            _parts[index].dataValues.parts.forEach(el=>{
+                data.dataValues.parts.push(el.part);
+            })
+            return data.dataValues
+        })
+        
+        res.send({data : result});
+    }catch(err){
+        res.status(500).send({message : 'server error'})
+    }
+
 }
