c01ad072fbdad684adaa5b8092de1d1bd9e8f9c7
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2021-01-24 17:09:49 +0900

    [fix]token검사 로직 추가

--- server/controllers/users/resign.js
+++ server/controllers/users/resign.js
@@ -1,19 +1,22 @@
 const { users } = require('../../models');
-
+const refreshKey = process.env.REFRESH_SECRET;
+const { verify } = require('jsonwebtoken');
 
 module.exports = async(req,res) =>{
-    const { email } = req.body // userName은 반드시 고유한 값이여야함
+    const { email } = req.body 
+    const userInfoInToken = req.headers.authorization.substr(7);
+    const checkToken = verify(userInfoInToken, refreshKey);
+    const checkEmail =await users.findOne({where : {email : email}});
 
-    const checkUserName =await users.findOne({where : {email : email}});
     try {
-        if(checkUserName){
+        if(checkEmail.email === checkToken.email){
             await users.destroy({ where : {email : email}});
             res.send({message : 'resign success'});
         }else{
             return res.status(400).send({message : 'cannot verified'});
         }
     }
-    catch(err){
-        return res.status(500).send({message : 'server error'});
-    } 
+    catch(err) {
+        return res.status(500).send({message: 'server error'})
+    }
 }
