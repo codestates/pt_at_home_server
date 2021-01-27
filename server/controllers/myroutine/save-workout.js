bb8cdd63ba6214814c20000b872edeaa881c67aa
somniumry <somniumry>
2021-01-28 05:40:57 +0900

    [Fix] redirect & token 수정

--- server/controllers/myroutine/save-workout.js
+++ server/controllers/myroutine/save-workout.js
@@ -5,44 +5,34 @@ const REFRESH_SECRET = process.env.REFRESH_SECRET
 const axios = require('axios')
 
 module.exports = async (req, res) => {
-    if (!req.headers.authorization) {
-        res.status(400).send({ message: 'accessToken not found' })
-    } else {
-        try {
-            const token = req.headers.authorization.split(" ")[2];
-            const accessVerify = jwt.verify(token, REFRESH_SECRET)
+    try {
+        const token = req.headers.authorization.substr(7);
+        const accessVerify = jwt.verify(token, REFRESH_SECRET)
 
-            const userInfo = await users.findOne({
-                attributes: ['id'],
-                where: { email: accessVerify.email }
-            }) // userId
-            console.log("++", userInfo)
+        const userInfo = await users.findOne({
+            attributes: ['id'],
+            where: { email: accessVerify.email }
+        }) // userId
 
-            const save = await user_workout.create({
-                userId: userInfo.dataValues.id,
-                workoutId: req.body.workoutId
-            }) // 조인테이블
-            // console.log("--", save.userId, save.workoutId)
+        const save = await user_workout.create({
+            userId: userInfo.dataValues.id,
+            workoutId: req.body.workoutId
+        }) // 조인테이블
 
-            const workoutAxios = await axios.get('http://localhost:8080/main')
-            const workoutList = workoutAxios.data.data;
-            // console.log("##", workoutList)
+        const workoutAxios = await axios.get('http://localhost:8080/main')
+        const workoutList = workoutAxios.data.data;
 
-            let data = [];
-            for(let i=0; i<workoutList.length; i++){
-                if(workoutList[i].id === req.body.workoutId) {
-                    data.push(workoutList[i])
-                }
+        let data = [];
+        for (let i = 0; i < workoutList.length; i++) {
+            if (workoutList[i].id === req.body.workoutId) {
+                data.push(workoutList[i])
             }
-            // console.log("@@", data)
-
-            res.status(200)
-            .send({
-                data, 
-                message: 'ok'
-            })
-        } catch (err) {
-            res.status(500).send("server error")
         }
+
+        res.status(200).redirect('http://localhost:8080/myroutine/myworkout')
+
+    } catch (err) {
+        res.status(500).send("server error")
     }
+
 }
