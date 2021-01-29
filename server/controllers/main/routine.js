4c702945e448a5220eb0485bd5783b23864cd6a8
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2021-01-29 14:12:59 +0900

    [feature]#10 /main/routine 기능구현 완료

--- server/controllers/main/routine.js
+++ server/controllers/main/routine.js
@@ -1,3 +1,8 @@
+const axios = require('axios');
+
 module.exports = async(req,res) =>{
-    
+    const recommendRoutine = await axios.get('http://localhost:8080/myroutine',{
+        headers : {withCredentials : true}
+    })
+    res.send({data : recommendRoutine.data.data, message : 'ok'})
 }
