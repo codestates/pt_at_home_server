935c405fed97164afd42d035d695af258fcc3580
Jmwasd <dktkqyd99@gmail.com>
2021-02-17 21:23:48 +0900

    Delete index.ts

--- savemehomt/src/controllers/main/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as main } from "./main";
-export { default as filter } from "./filter";
-export { default as search } from "./search";
-export { default as routine } from "./routine";
